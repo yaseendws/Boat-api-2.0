c19e0f80c611ef6d0c82e183763002b04fcb6692
DeskWorkSold <desksoldevs@gmail.com>
2023-11-06 12:20:16 +0500

    d

--- src/helper/response.js
+++ src/helper/response.js
@@ -6,14 +6,14 @@ const responseHandler = (res,data) =>{
 }
 const errHandler = (res,err,status) =>{
     console.log(typeof err)
-    res.status(status)
     if(typeof err=="string"){
+        throw new err
         res.json({err})
-        throw new err
     }else{
-        res.json({err:errList[err]})
         throw new err
+        res.json({err:errList[err]})
     }
+    res.status(status)
 }
 
 export {responseHandler,errHandler}
